f3c81a55a463ec88c45d80bff88787eb415ae250
janildoss <janildosantos@yahoo.com.br>
2020-03-05 16:27:42 -0300

    Projeto iniciado com crud de departments,migration e bd mysql

---
+++ SalesWebMvc_n/Data/SalesWebMvc_nContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc_n.Models;
+
+namespace SalesWebMvc_n.Data
+{
+    public class SalesWebMvc_nContext : DbContext
+    {
+        public SalesWebMvc_nContext (DbContextOptions<SalesWebMvc_nContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc_n.Models.Department> Department { get; set; }
+    }
+}
